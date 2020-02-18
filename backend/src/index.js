e728b3cda8461c810c262f1f44132ba3b5c5a963
caue96 <cauemateus@hotmail.com>
2020-02-18 18:42:08 -0300

    AGORA VAI!

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,12 @@ const routes = require("./routes");
 
 const app = express();
 
+app.listen(3333);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
-
-mongoose.connect("mongodb://localhost:27017/class", {
+mongoose.connect("mongodb://localhost:27017/repo_aula_bd", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
